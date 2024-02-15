068e1596fb2ce0b2f3b8ee2efa9081870512960a
Joel Filho <joelfilho02812@gmail.com>
2024-02-15 10:18:06 -0300

    fix: alignment adjustments in products index page

--- frontend/burger-shop/src/views/products/IndexPage.vue
+++ frontend/burger-shop/src/views/products/IndexPage.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { Card, CardContent} from '@/components/ui/card'
+import { Card, CardContent } from '@/components/ui/card'
 import SelectField from '@/components/SelectField.vue'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
@@ -11,32 +11,45 @@ import { Label } from '@/components/ui/label'
     <div>
       <img class="w-lvw" src="../../assets/bg-burger-products.svg" />
     </div>
-    <div class="flex gap-5 items-center self-start ml-[11vw]">
-      <div class="mt-5">
-        <h3>Filters:</h3>
-      </div>
-      <div class="flex gap-2">
-        <div>
-          <Label>Name</Label>
-          <Input placeholder="Name" />
-        </div>
-        <div>
-          <SelectField
-            id="category"
-            label="Category"
-            item-value="id"
-            item-text="description"
-            :items="[]"
-          />
+
+    <div class="self-start ml-[9vw]">
+      <div>
+        <h1 class="font-bold font-mono text-4xl">Products</h1>
+        <div class="flex items-center gap-5 ml-5 mt-5">
+          <div class="mt-5">
+            <h3>Filters:</h3>
+          </div>
+          <div class="flex gap-2">
+            <div>
+              <Label>Name</Label>
+              <Input placeholder="Name" />
+            </div>
+            <div>
+              <SelectField
+                id="category"
+                label="Category"
+                item-value="id"
+                item-text="description"
+                :items="[]"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
     <div class="grid grid-cols-3 gap-5">
-      <Card class="max-w-[30vw] max-h-[25vh] border-violet-950" v-for="(_, index) in 9" :key="index">
+      <Card
+        class="max-w-[30vw] max-h-[25vh] border-violet-950"
+        v-for="(_, index) in 9"
+        :key="index"
+      >
         <CardContent>
           <div class="flex items-center justify-center gap-5">
             <div class="pt-5">
-              <img class="rounded-md relative right-2 h-[20vh] w-full" src="../../assets/bg-burger-login.svg" />
+              <img
+                class="rounded-md relative right-2 h-[20vh] w-full"
+                src="../../assets/bg-burger-login.svg"
+              />
             </div>
 
             <div class="flex flex-col justify-around h-[20vh] mt-5">
@@ -47,9 +60,7 @@ import { Label } from '@/components/ui/label'
                 </div>
               </div>
               <div>
-                <Button class="w-full font-medium">
-                  Adicionar
-                </Button>
+                <Button class="w-full font-medium"> Adicionar </Button>
               </div>
             </div>
           </div>
