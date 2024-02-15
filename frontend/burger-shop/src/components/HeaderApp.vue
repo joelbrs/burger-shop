eb8f04fdbbd9798c80b9fc5f3b2b215efccfb35d
Joel Filho <joelfilho02812@gmail.com>
2024-02-15 10:16:43 -0300

    fix: header

--- frontend/burger-shop/src/components/HeaderApp.vue
+++ frontend/burger-shop/src/components/HeaderApp.vue
@@ -4,12 +4,25 @@ import { RouterLink } from 'vue-router'
 import { Separator } from '@/components/ui/separator'
 import { ShoppingCart } from 'lucide-vue-next'
 import BtnUserInfos from '@/components/BtnUserInfos.vue'
+import { onMounted, ref } from 'vue'
+
+type RouteName = 'home' | 'products'
 
 const $route = useRoute()
 
-const isHome = $route.name === 'home'
-const isCart = $route.name === 'cart'
-const isProduct = $route.name === 'home-products' || $route.name === 'cart'
+const active = ref({
+  home: false,
+  products: false
+})
+
+const activate = ({ name }: { name: RouteName }) => {
+  for (const route in active.value) {
+    if (route !== name) {
+      active.value[route as keyof typeof active.value] = false
+    }
+  }
+  active.value[name as keyof typeof active.value] = true
+}
 
 const setActive = (isCurrent: boolean) => {
   if (isCurrent) {
@@ -17,6 +30,14 @@ const setActive = (isCurrent: boolean) => {
   }
   return 'hover:cursor-pointer hover:hover:text-violet-700'
 }
+
+onMounted(() => {
+  const { name } = $route
+
+  if (name) {
+    activate({ name: name as RouteName })
+  }
+})
 </script>
 
 <template>
@@ -25,18 +46,22 @@ const setActive = (isCurrent: boolean) => {
       <div class="flex justify-between px-16 items-center">
         <div>
           <ul class="flex gap-5">
-            <li :class="`text-md text-black font-serif ${setActive(isHome)}`">
-              <RouterLink :to="{ name: 'home' }">Home</RouterLink>
+            <li :class="`text-md text-black font-serif ${setActive(active.home)}`">
+              <RouterLink @click="activate({ name: 'home' })" :to="{ name: 'home' }">
+                Home
+              </RouterLink>
             </li>
-            <li :class="`text-md text-black font-serif ${setActive(isProduct)}`">
-              <RouterLink :to="{ name: 'home-products' }">Products</RouterLink>
+            <li :class="`text-md text-black font-serif ${setActive(active.products)}`">
+              <RouterLink @click="activate({ name: 'products' })" :to="{ name: 'products' }">
+                Products
+              </RouterLink>
             </li>
           </ul>
         </div>
         <div class="flex gap-5 h-7 items-center">
           <div class="text-violet-700">
             <RouterLink :to="{ name: 'home' }">
-              <ShoppingCart :class="setActive(isCart)" />
+              <ShoppingCart />
             </RouterLink>
           </div>
           <Separator orientation="vertical" />
